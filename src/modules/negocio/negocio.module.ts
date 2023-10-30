3ac99def1d4536ba0f1b3b0836215904c08192d3
santinajar <s.najar@uniandes.edu.co>
2023-10-30 10:19:35 -0500

    modulo negocio

---
+++ src/modules/negocio/negocio.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NegocioComponent } from './negocio.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [NegocioComponent]
+})
+export class NegocioModule { }
